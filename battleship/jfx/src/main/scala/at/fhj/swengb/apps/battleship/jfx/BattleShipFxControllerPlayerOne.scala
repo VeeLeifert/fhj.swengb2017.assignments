5dad499efda88d2ac09c77181c74e166e3864711
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2018-01-18 12:38:54 +0100

    Working on bugfixes

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxControllerPlayerOne.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxControllerPlayerOne.scala
@@ -4,10 +4,14 @@ import java.net.URL
 import java.util.ResourceBundle
 import javafx.fxml.{FXML, Initializable}
 
+import javafx.scene.control._
+
 class BattleShipFxControllerPlayerOne extends Initializable {
 
-  override def initialize(url: URL, rb: ResourceBundle): Unit = {
+  @FXML var Title: Label = _
 
+  override def initialize(url: URL, rb: ResourceBundle): Unit = {
+    Title.setText(BattleShipFxControllerCreateGame.battleName ++ " - " ++ BattleShipFxControllerCreateGame.playerOne)
   }
 
   @FXML def toWelcome(): Unit = BattleShipFxApp.ScenePresenter3000(BattleShipFxApp.SceneLoader3000("/at/fhj/swengb/apps/battleship/jfx/welcomescreen.fxml"),BattleShipFxApp.FirstStage3000)
