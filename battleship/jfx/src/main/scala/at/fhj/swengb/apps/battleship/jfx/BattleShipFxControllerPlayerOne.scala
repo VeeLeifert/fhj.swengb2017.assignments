9d128b90e18f6cdcd2ab1120df19974c97ea8814
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2018-01-18 14:15:51 +0100

    Saving Function

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxControllerPlayerOne.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxControllerPlayerOne.scala
@@ -3,12 +3,20 @@ package at.fhj.swengb.apps.battleship.jfx
 import java.net.URL
 import java.util.ResourceBundle
 import javafx.fxml.{FXML, Initializable}
-
 import javafx.scene.control._
+import javafx.stage.{FileChooser, Stage}
+import javafx.stage.FileChooser.ExtensionFilter
+import java.io.File
+import java.nio.file.{Files, Paths}
+
+import at.fhj.swengb.apps.battleship.BattleShipProtocol
+import at.fhj.swengb.apps.battleship.model.PlayerField
 
 class BattleShipFxControllerPlayerOne extends Initializable {
 
+  @FXML var log: TextArea = _
   @FXML var Title: Label = _
+  private var Game: PlayerField = _
 
   override def initialize(url: URL, rb: ResourceBundle): Unit = {
     Title.setText(BattleShipFxControllerCreateGame.battleName ++ " - " ++ BattleShipFxControllerCreateGame.playerOne)
@@ -19,7 +27,16 @@ class BattleShipFxControllerPlayerOne extends Initializable {
   @FXML def toPlayerTwo(): Unit = BattleShipFxApp.ScenePresenter3000(BattleShipFxApp.SceneLoader3000("/at/fhj/swengb/apps/battleship/jfx/playertwoscreen.fxml"),BattleShipFxApp.FirstStage3000)
 
   @FXML def saveGame(): Unit = {
+      //Using FileChooser for accessing our files
+      val FileChooser3000 = new FileChooser();
+      //Filtering on our protobuf files with the ending .bin
+      val ProtoFilter3000: FileChooser.ExtensionFilter = new ExtensionFilter("Protobuf files","*.bin")
+      FileChooser3000.getExtensionFilters.add(ProtoFilter3000)
+      //Converting and saving
+      val FileSaver3000: File = FileChooser3000.showSaveDialog(BattleShipFxApp.FirstStage3000)
+      BattleShipProtocol.convert(Game).writeTo(Files.newOutputStream(Paths.get(FileSaver3000.getAbsolutePath)))
+      log.appendText("\n" ++ "Saved Game")
+    }
 
-  }
 
 }
